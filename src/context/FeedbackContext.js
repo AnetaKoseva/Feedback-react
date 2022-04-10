3e165c1181da7ae7bbc2dca392fac6c0b209f53d
AnetaKoseva <62696878+AnetaKoseva@users.noreply.github.com>
2022-04-10 22:28:34 +0300

    data is loading from server-db.server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,42 +1,41 @@
 import { v4 as uuidv4 } from 'uuid'
-import {createContext,useState} from 'react'
+import {createContext,useState, useEffect} from 'react'
 
 const FeedbackContext=createContext();
 
 export const FeedbackProvider=({children})=>{
-    const [feedback,setFeedback]=useState([
-        {
-            id: 1,
-            rating: 10,
-            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
-          },
-          {
-            id: 2,
-            rating: 9,
-            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
-          },
-          {
-            id: 3,
-            rating: 8,
-            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
-          },
-    ]);
+    const [feedback,setFeedback]=useState([]);
 
     const [feedbackEdit,setFeedbackEdit]=useState({
         item:{},
         edit: false,
     });
+
+    useEffect(()=>{
+        fetchFeedback()
+    },[])
+    
+//Fetch feedback
+const fetchFeedback=async()=>{
+    const response=await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`)
+    const data=await response.json()
+
+    setFeedback(data)
+}
+
 //Add feedback
     const addFeedback = (newFeedback) => {
         newFeedback.id = uuidv4();
         setFeedback([newFeedback, ...feedback])
       }
+
 //Delete feedback
     const deleteFeedback = (id) => {
         if (window.confirm('Are you sure you want to delete?')) {
           setFeedback(feedback.filter((item) => item.id !== id))
         }
       }
+
 //Update feedback item
 const updateFeedback=(id,upItem)=>{
     setFeedback(feedback.map((item)=>item.id===id?{item, ...upItem}:item))
